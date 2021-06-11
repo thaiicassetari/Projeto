c8aeaa81662cdb4607b6a413ff235cfd4b831748
Thaiane <you@example.com>
2021-06-11 17:20:39 -0300

    Menu principal puxado as outras páginas

---
+++ menu.php
@@ -0,0 +1,10 @@
+<header>
+    <h1>♛ Logo ♛</h1>
+</header> 
+<div class="menu"> <!--menu-->
+    <a href="index.php">HOME</a> |
+        <a href="cadastro.php">CADASTRO / ENTRADA</a> |
+        <a href="saida.php">SAÍDA</a> |
+        <a href="CAIXA.php">CAIXA</a> |
+        <a href="relatorio.php">RELATÓRIOS</a>  
+</div>
